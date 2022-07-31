7ec9ee158ea4479409372d549a2219f0f761b2cc
jy@mykine <mykine@sina.com>
2022-08-01 00:36:02 +0800

    定义广告流量类型常量

--- mykine-ad-service/ad-search/src/main/java/cn/mykine/ad/index/adunit/AdUnitConstants.java
+++ mykine-ad-service/ad-search/src/main/java/cn/mykine/ad/index/adunit/AdUnitConstants.java
@@ -2,15 +2,16 @@ package cn.mykine.ad.index.adunit;
 
 /**
  * Created by Jo@mykine
+ * 流量类型
  */
 public class AdUnitConstants {
 
     public static class POSITION_TYPE {
 
-        public static final int KAIPING = 1;
-        public static final int TIEPIAN = 2;
-        public static final int TIEPIAN_MIDDLE = 4;
-        public static final int TIEPIAN_PAUSE = 8;
-        public static final int TIEPIAN_POST = 16;
+        public static final int KAIPING = 1;//开屏广告
+        public static final int TIEPIAN = 2;//视频播放前的贴片广告
+        public static final int TIEPIAN_MIDDLE = 4;//中贴，视屏播放的中间植入的广告
+        public static final int TIEPIAN_PAUSE = 8;//暂停贴，暂停播放时植入的广告
+        public static final int TIEPIAN_POST = 16;//后贴，播放完后植入的广告
     }
 }
